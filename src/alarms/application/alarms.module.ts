4327ca1a980e9f730d2ff00c170b3668cf21abbb
Alexandru Constantinov <kottinov@gmail.com>
2023-12-14 20:03:10 +0200

    Add GetAlarmsQueryHandler to AlarmsModule

--- src/alarms/application/alarms.module.ts
+++ src/alarms/application/alarms.module.ts
@@ -3,10 +3,16 @@ import { AlarmFactory } from '../domain/factories/alarm.factory';
 import { AlarmsController } from '../presenters/http/alarms.controller';
 import { AlarmsService } from './alarms.service';
 import { CreateAlarmCommandHandler } from './commands/create-alarm.command-handler';
+import { GetAlarmsQueryHandler } from './queries/get-alarms.query-handler';
 
 @Module({
   controllers: [AlarmsController],
-  providers: [AlarmsService, AlarmFactory, CreateAlarmCommandHandler],
+  providers: [
+    AlarmsService,
+    AlarmFactory,
+    CreateAlarmCommandHandler,
+    GetAlarmsQueryHandler,
+  ],
 })
 export class AlarmsModule {
   static withInfrastructure(infrastructureModule: Type | DynamicModule) {
