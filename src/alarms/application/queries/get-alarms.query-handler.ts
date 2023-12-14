8728bf5c8af0794766ea66552708f9c92197187f
Alexandru Constantinov <kottinov@gmail.com>
2023-12-14 20:06:03 +0200

    Add logger to GetAlarmsQueryHandler

--- src/alarms/application/queries/get-alarms.query-handler.ts
+++ src/alarms/application/queries/get-alarms.query-handler.ts
@@ -2,6 +2,7 @@ import { IQueryHandler, QueryHandler } from '@nestjs/cqrs';
 import { GetAlarmsQuery } from './get-alarms.query';
 import { Alarm } from 'src/alarms/domain/alarm';
 import { AlarmRepository } from '../ports/alarm.repository';
+import { Logger } from '@nestjs/common';
 
 @QueryHandler(GetAlarmsQuery)
 export class GetAlarmsQueryHandler
@@ -9,7 +10,10 @@ export class GetAlarmsQueryHandler
 {
   constructor(private readonly alarmRepository: AlarmRepository) {}
 
+  private readonly logger = new Logger(GetAlarmsQueryHandler.name);
+
   async execute(query: GetAlarmsQuery): Promise<Alarm[]> {
+    this.logger.debug(`Processing get alarms query: ${JSON.stringify(query)}`);
     return this.alarmRepository.findAll();
   }
 }
