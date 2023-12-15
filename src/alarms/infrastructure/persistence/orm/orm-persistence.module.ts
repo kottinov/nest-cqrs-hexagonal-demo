d331f8d388b5eb8b2a814b12843dc6aac62b8f2a
Alexandru Constantinov <kottinov@gmail.com>
2023-12-15 23:27:51 +0200

    Refactor alarm persistence module imports and providers

--- src/alarms/infrastructure/persistence/orm/orm-persistence.module.ts
+++ src/alarms/infrastructure/persistence/orm/orm-persistence.module.ts
@@ -2,17 +2,43 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CreateAlarmRepository } from 'src/alarms/application/ports/create-alarm.repository';
 import { AlarmEntity } from './entities/alarm-entity';
-import { OrmAlarmRepository } from './repositories/alarm.repository';
+import { OrmCreateAlarmRepository } from './repositories/create-alarm.repository';
 import { AlarmItemEntity } from './entities/alarm-item.entity';
+import { FindAlarmsRepository } from 'src/alarms/application/ports/find-alarm.repository';
+import { OrmFindAlarmsRepository } from './repositories/find-alarms.repository';
+import { UpsertMaterializedAlarmRepository } from 'src/alarms/application/ports/upsert-materialized-alarm.repository';
+import { OrmUpsertMaterializedAlarmRepository } from './repositories/upsert-materialized-alarm.repository';
+import { MongooseModule } from '@nestjs/mongoose';
+import {
+  MaterializedAlarmView,
+  MaterializedAlarmViewSchema,
+} from './schemas/materialized-alarm-view.schema';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([AlarmEntity, AlarmItemEntity])],
+  imports: [
+    TypeOrmModule.forFeature([AlarmEntity, AlarmItemEntity]),
+    MongooseModule.forFeature([
+      { name: MaterializedAlarmView.name, schema: MaterializedAlarmViewSchema },
+    ]),
+  ],
   providers: [
     {
       provide: CreateAlarmRepository,
-      useClass: OrmAlarmRepository,
+      useClass: OrmCreateAlarmRepository,
+    },
+    {
+      provide: FindAlarmsRepository,
+      useClass: OrmFindAlarmsRepository,
+    },
+    {
+      provide: UpsertMaterializedAlarmRepository,
+      useClass: OrmUpsertMaterializedAlarmRepository,
     },
   ],
-  exports: [CreateAlarmRepository],
+  exports: [
+    CreateAlarmRepository,
+    FindAlarmsRepository,
+    UpsertMaterializedAlarmRepository,
+  ],
 })
 export class OrmAlarmPersistenceModule {}
