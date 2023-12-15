daed37f5844fe001cf5cbf94cdb8e21362723e81
Alexandru Constantinov <kottinov@gmail.com>
2023-12-15 23:27:22 +0200

    Fix typo in Alarm class variable name

--- src/alarms/domain/alarm.ts
+++ src/alarms/domain/alarm.ts
@@ -5,13 +5,13 @@ export class Alarm {
   public name: string;
   public severity: AlarmSeverity;
   public triggeredAt: Date;
-  public isAcknowldedged = false;
+  public isAcknowledged = false;
   public items = new Array<AlarmItem>();
 
   constructor(public id: string) {}
 
   acknowledge() {
-    this.isAcknowldedged = true;
+    this.isAcknowledged = true;
   }
 
   addAlarmItem(item: AlarmItem) {
