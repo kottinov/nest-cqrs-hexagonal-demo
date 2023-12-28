0a7476f02eb0b0f724663091b43a2ae89328544d
Alexandru Constantinov <kottinov@gmail.com>
2023-12-28 10:24:15 +0200

    Add EventPublisher to CreateAlarmCommandHandler

--- src/alarms/application/commands/create-alarm.command-handler.ts
+++ src/alarms/application/commands/create-alarm.command-handler.ts
@@ -1,4 +1,9 @@
-import { CommandHandler, EventBus, ICommandHandler } from '@nestjs/cqrs';
+import {
+  CommandHandler,
+  EventBus,
+  EventPublisher,
+  ICommandHandler,
+} from '@nestjs/cqrs';
 import { CreateAlarmCommand } from './create-alarm.command';
 import { Logger } from '@nestjs/common';
 import { AlarmFactory } from 'src/alarms/domain/factories/alarm.factory';
@@ -12,9 +17,8 @@ export class CreateAlarmCommandHandler
   private readonly logger = new Logger(CreateAlarmCommandHandler.name);
 
   constructor(
-    private readonly alarmRepository: CreateAlarmRepository,
+    private readonly eventPublisher: EventPublisher,
     private readonly alarmFactory: AlarmFactory,
-    private readonly eventBus: EventBus,
   ) {}
 
   async execute(command: CreateAlarmCommand) {
@@ -27,9 +31,10 @@ export class CreateAlarmCommandHandler
       command.triggeredAt,
       command.items,
     );
-    const newAlarm = await this.alarmRepository.save(alarm);
 
-    this.eventBus.publish(new AlarmCreatedEvent(alarm));
-    return newAlarm;
+    this.eventPublisher.mergeObjectContext(alarm);
+    alarm.commit();
+
+    return alarm;
   }
 }
