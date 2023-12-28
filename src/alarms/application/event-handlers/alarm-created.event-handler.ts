aa8439a27a819e1e5805511174e230201895cfda
Alexandru Constantinov <kottinov@gmail.com>
2023-12-28 12:43:22 +0200

    Fix import typo in AlarmCreatedEventHandler

--- src/alarms/application/event-handlers/alarm-created.event-handler.ts
+++ src/alarms/application/event-handlers/alarm-created.event-handler.ts
@@ -2,11 +2,11 @@ import { Logger } from '@nestjs/common';
 import { EventsHandler, IEventHandler } from '@nestjs/cqrs';
 import { AlarmCreatedEvent } from 'src/alarms/domain/events/alarm-created.event';
 import { UpsertMaterializedAlarmRepository } from '../ports/upsert-materialized-alarm.repository';
-import { SerializedEventPaylod } from 'src/shared/domain/interfaces/serializable-event';
+import { SerializedEventPayload } from 'src/shared/domain/interfaces/serializable-event';
 
 @EventsHandler(AlarmCreatedEvent)
 export class AlarmCreatedEventHandler
-  implements IEventHandler<SerializedEventPaylod<AlarmCreatedEvent>>
+  implements IEventHandler<SerializedEventPayload<AlarmCreatedEvent>>
 {
   private readonly logger = new Logger(AlarmCreatedEventHandler.name);
 
@@ -14,13 +14,13 @@ export class AlarmCreatedEventHandler
     private readonly upsertMaterializedAlarmRepository: UpsertMaterializedAlarmRepository,
   ) {}
 
-  async handle(event: SerializedEventPaylod<AlarmCreatedEvent>) {
+  async handle(event: SerializedEventPayload<AlarmCreatedEvent>) {
     this.logger.log(`Alarm created event: ${JSON.stringify(event)}`);
 
     await this.upsertMaterializedAlarmRepository.upsert({
       id: event.alarm.id,
       name: event.alarm.name,
-      severity: event.alarm.severity.value,
+      severity: event.alarm.severity,
       triggeredAt: new Date(event.alarm.triggeredAt),
       isAcknowledged: event.alarm.isAcknowledged,
       items: event.alarm.items,
