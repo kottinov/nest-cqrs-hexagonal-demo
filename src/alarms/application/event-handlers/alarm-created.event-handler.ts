1a04dd5c685c1d08c9ffc4eb8aba06eb4fff9482
Alexandru Constantinov <kottinov@gmail.com>
2023-12-28 10:53:08 +0200

    Refactor AlarmCreatedEventHandler to use SerializedEventPaylod

--- src/alarms/application/event-handlers/alarm-created.event-handler.ts
+++ src/alarms/application/event-handlers/alarm-created.event-handler.ts
@@ -2,10 +2,11 @@ import { Logger } from '@nestjs/common';
 import { EventsHandler, IEventHandler } from '@nestjs/cqrs';
 import { AlarmCreatedEvent } from 'src/alarms/domain/events/alarm-created.event';
 import { UpsertMaterializedAlarmRepository } from '../ports/upsert-materialized-alarm.repository';
+import { SerializedEventPaylod } from 'src/shared/domain/interfaces/serializable-event';
 
 @EventsHandler(AlarmCreatedEvent)
 export class AlarmCreatedEventHandler
-  implements IEventHandler<AlarmCreatedEvent>
+  implements IEventHandler<SerializedEventPaylod<AlarmCreatedEvent>>
 {
   private readonly logger = new Logger(AlarmCreatedEventHandler.name);
 
@@ -13,14 +14,14 @@ export class AlarmCreatedEventHandler
     private readonly upsertMaterializedAlarmRepository: UpsertMaterializedAlarmRepository,
   ) {}
 
-  async handle(event: AlarmCreatedEvent) {
+  async handle(event: SerializedEventPaylod<AlarmCreatedEvent>) {
     this.logger.log(`Alarm created event: ${JSON.stringify(event)}`);
 
     await this.upsertMaterializedAlarmRepository.upsert({
       id: event.alarm.id,
       name: event.alarm.name,
       severity: event.alarm.severity.value,
-      triggeredAt: event.alarm.triggeredAt,
+      triggeredAt: new Date(event.alarm.triggeredAt),
       isAcknowledged: event.alarm.isAcknowledged,
       items: event.alarm.items,
     });
